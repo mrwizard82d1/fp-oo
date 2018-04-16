b3140b98b7e39cb8cde09a7a7847c401b17389a9
Larry Jones <mrwizard82d1@gmail.com>
2018-04-16 04:09:02 -0500

    Update project.clj
    - Clojure 1.9.0
    - Include `midje`

--- project.clj
+++ project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :profiles {:dev {:dependencies [[midje "1.9.1"]]}
+             ;; You can add dependencies that apply to `lein midje` below.
+             ;; An example would be changing the logging destination for test runs.
+             :midje {}})
+             ;; Note that Midje itself is in the `dev` profile to support
+             ;; running autotest in the repl.
