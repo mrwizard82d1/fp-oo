393452f6fa91fd904c51aeb8d3b2c5d8d8e3faae
Larry Jones <larry.jones@reveal-energy.com>
2018-04-16 19:43:51 -0500

    Two different implementations of `third`

--- src/fp_oo/just_enough_clojure.clj
+++ src/fp_oo/just_enough_clojure.clj
@@ -1,5 +1,13 @@
 (ns fp-oo.just-enough-clojure)
 
-(def second
+(def my-second
   (fn [l]
     (first (rest l))))
+
+(def third-a
+  (fn [l]
+    (first (rest (rest l)))))
+
+(def third-b
+  (fn [l]
+    (nth l 2 nil)))
--- test/fp_oo/just_enough_clojure_test.clj
+++ test/fp_oo/just_enough_clojure_test.clj
@@ -3,6 +3,16 @@
             [fp-oo.just-enough-clojure :as jec]))
 
 (facts "second"
-       (fact (jec/second []) => empty?)
-       (fact (jec/second [:a]) => empty?)
-       (fact (jec/second [:a :b]) => :b))
+       (fact (jec/my-second []) => empty?)
+       (fact (jec/my-second [:a]) => empty?)
+       (fact (jec/my-second [:a :b]) => :b))
+
+(facts "third"
+       (fact (jec/third-a []) => empty?)
+       (fact (jec/third-a [:a]) => empty?)
+       (fact (jec/third-a [:a :b]) => empty?)
+       (fact (jec/third-a [:a :b :c]) => :c)
+       (fact (jec/third-b []) => empty?)
+       (fact (jec/third-b [:a]) => empty?)
+       (fact (jec/third-b [:a :b]) => empty?)
+       (fact (jec/third-b [:a :b :c]) => :c))
