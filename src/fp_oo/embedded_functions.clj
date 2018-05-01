24898c4ac47adfedf3ebcb6955aafb5d2ca1acf0
Larry Jones <larry.jones@reveal-energy.com>
2018-04-30 21:12:30 -0500

    Copy add_and_make to embedded_functions

---
+++ src/fp_oo/embedded_functions.clj
@@ -0,0 +1,55 @@
+(ns fp-oo.embedded-functions
+  (:require [fp-oo.just-enough-clojure :as jec]))
+
+(def Point
+  (fn [x y]
+    {:x x
+     :y y
+     :__class-symbol__ 'Point}))
+
+(def x :x)
+
+(def y :y)
+
+(def class-of :__class-symbol__)
+
+(def shift
+  (fn [this x-inc y-inc]
+    (Point (+ (x this) x-inc)
+           (+ (y this) y-inc))))
+
+(def add
+  (fn [this addend]
+    (shift this (x addend) (y addend))))
+
+(def Triangle
+  (fn [v1 v2 v3]
+    {:v1 v1
+     :v2 v2
+     :v3 v3
+     :__class-symbol__ 'Triangle}))
+
+(def make
+  (fn [ctor & args]
+    (apply ctor args)))
+
+(def equal-triangles?
+  (fn [& args]
+    (apply = args)))
+
+(def right-triangle
+  (make Triangle (make Point 0 0) (make Point 1 0) (make Point 1 1)))
+
+(def equal-right-triangle
+  (make Triangle (make Point 0 0) (make Point 1 0) (make Point 1 1)))
+
+(def different-right-triangle
+  (make Triangle (make Point 0 0) (make Point 1 0) (make Point 1 2)))
+
+(def v1 :v1)
+(def v2 :v2)
+(def v3 :v3)
+
+(def valid-triangle?
+  (fn [v1 v2 v3]
+    (zero? (jec/duplicate-count [v1 v2 v3]))))
