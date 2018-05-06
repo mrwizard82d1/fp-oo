ab86d060e23bc880b0c13e243f632af8b75728b9
Larry Jones <larry.jones@reveal-energy.com>
2018-05-06 13:32:11 -0500

    Write `send-to`

--- src/fp_oo/class.clj
+++ src/fp_oo/class.clj
@@ -7,8 +7,14 @@
           constructor (:add-instance-values (:__instance-methods__ klass))]
       (apply constructor seeded args))))
 
+(def send-to
+  (fn [instance message & args]
+    (let [klass (eval (:__class-symbol__ instance))
+          method (message (:__instance-methods__ klass))]
+      (apply method instance args))))
+
 (def Point
-  {:__own-symbol__ 'Point
+  {:__own-symbol__ `Point
    :__instance-methods__
    {:class :__class-symbol__
     :add-instance-values
--- test/fp_oo/class_test.clj
+++ test/fp_oo/class_test.clj
@@ -5,4 +5,9 @@
 (facts "make"
   (let [to-test (foc/make foc/Point 743 803)]
     (fact to-test => (contains {:x 743 :y 803}))
-    (fact to-test => (contains {:__class-symbol__ 'Point}))))
+    (fact to-test => (contains {:__class-symbol__ `foc/Point}))))
+
+(facts "send-to"
+  (let [to-test (foc/make foc/Point 168 273)]
+    (fact (foc/send-to to-test :shift -2 -3) => 
+          (contains {:x 166 :y 270 :__class-symbol__ `foc/Point}))))
