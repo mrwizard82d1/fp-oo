d2e8a13d1e04642f3b5c6dd842fd4a8b562c3314
Larry Jones <larry.jones@reveal-energy.com>
2018-05-07 20:51:35 -0500

    Write Point with :add and :shift

--- src/fp_oo/java.clj
+++ src/fp_oo/java.clj
@@ -6,10 +6,23 @@
           constructor (:add-instance-values (:__instance-methods__ klass))]
       (apply constructor seeded args))))
 
+(def send-to
+  (fn [this message & args]
+    (let [method (message (:__instance-methods__ (eval (:__class-symbol__ this))))]
+      (apply method this args))))
+
 (def Point 
   {:__own-symbol__ `Point
    :__instance-methods__ 
    {:add-instance-values
     (fn [this x y]
-      (assoc this :x x :y y))}})
+      (assoc this :x x :y y))
+    :add
+    (fn [this addend]
+      (send-to this :shift (:x addend) (:y addend)))
+    :shift
+    (fn [this delta-x delta-y]
+      (make Point 
+            (+ (:x this) delta-x)
+            (+ (:y this) delta-y)))}})
 
--- test/fp_oo/java_test.clj
+++ test/fp_oo/java_test.clj
@@ -12,3 +12,9 @@
     (fact to-test => (contains {:x 885 :y 757}))
     (fact another => (contains {:x 347 :y 84}))
     (fact to-test => (contains {:__class-symbol__ `java/Point}))))
+
+(facts "send-to"
+  (let [to-test (java/make java/Point 511 631)
+        addend (java/make java/Point 659 783)]
+    (fact (java/send-to to-test :shift -2 -3) => (contains {:x 509 :y 628}))
+    (fact (java/send-to to-test :add addend) => (contains {:x 1170 :y 1414}))))
