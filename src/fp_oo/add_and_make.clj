0aebf26be22df96b2f5a867e71361d792dc9b601
Larry Jones <larry.jones@reveal-energy.com>
2020-06-03 00:33:22 -0500

    Write barely believable object

--- src/fp_oo/add_and_make.clj
+++ src/fp_oo/add_and_make.clj
@@ -1,4 +1,21 @@
 (ns fp-oo.add-and-make)
 
 (defn Point [x y]
-  {:x x :y y})
+  {;; instance data
+   :x x
+   :y y
+   ;; "metadata"
+   :__class_symbol__ 'Point})
+
+(defn x [this] (:x this))
+
+(defn y [this] (:y this))
+
+(defn class-of [this]
+  (:__class_symbol__ this))
+
+(defn shift [this x-increment y-increment]
+  (print (x this))
+  (Point (+ (x this) x-increment)
+         (+ (y this) y-increment)))
+
--- test/fp_oo/add_and_make_test.clj
+++ test/fp_oo/add_and_make_test.clj
@@ -3,4 +3,12 @@
   (:require fp-oo.add-and-make))
 
 (fact "construct Point"
-      (fp-oo.add-and-make/Point 62 81) => {:x 62 :y 81})
+      (fp-oo.add-and-make/x (fp-oo.add-and-make/Point 62 81)) => 62
+      (fp-oo.add-and-make/y (fp-oo.add-and-make/Point 62 81)) => 81
+      (fp-oo.add-and-make/class-of (fp-oo.add-and-make/Point 62 81)) => 'Point
+      (-> (fp-oo.add-and-make/Point 62 81)
+          (fp-oo.add-and-make/shift 5 8)
+          (fp-oo.add-and-make/x)) => 67
+      (-> (fp-oo.add-and-make/Point 62 81)
+          (fp-oo.add-and-make/shift 5 8)
+          (fp-oo.add-and-make/y)) => 89)
