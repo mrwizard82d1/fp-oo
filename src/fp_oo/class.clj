710e6f8224a0fc8c78bff3f8d77f867c0e3024c7
Larry Jones <larry.jones@reveal-energy.com>
2020-06-05 20:35:53 -0500

    Write `fo-oo.class/send-to`

--- src/fp_oo/class.clj
+++ src/fp_oo/class.clj
@@ -1,15 +1,23 @@
 (ns fp-oo.class)
 
-(defn make [class-map & args]
-  (let [allocated-instance {}
-        seeded-instance (assoc allocated-instance :__class_symbol__ (:__own_symbol__ class-map))
-        constructor (get-in class-map [:__instance_methods__ :add-instance-values])
-        initialized-instance (apply constructor seeded-instance args)]
-    initialized-instance))
+(defn make [klass & args]
+  (let [seeded-instance {:__class_symbol__ (:__own_symbol__ klass)}
+        constructor (get-in klass [:__instance_methods__ :add-instance-values])]
+    (apply constructor seeded-instance args)))
+
+(defn local-eval [x]
+  (binding [*ns* (find-ns 'fp-oo.class)]
+    (eval x)))
+
+(defn send-to [this message & args]
+  (let [klass (local-eval (:__class_symbol__ this))
+        method (get-in klass [:__instance_methods__ message])]
+    (apply method this args)))
 
 (def Point {:__own_symbol__ 'Point
             :__instance_methods__ {:add-instance-values (fn [this x y]
                                                           (assoc this :x x :y y))
+                                   :class :__class_symbol__
                                    :shift (fn [this x-increment y-increment]
                                             (make Point
                                                   (+ (:x this) x-increment)
--- test/fp_oo/class_test.clj
+++ test/fp_oo/class_test.clj
@@ -5,8 +5,25 @@
 (facts "canary"
        (+ 2 2) => 4)
 
+(facts "Point (meta) class"
+       (let [sut foc/Point]
+         (set (keys sut)) => #{:__instance_methods__ :__own_symbol__}
+         (set (keys (:__instance_methods__ sut))) => #{:add-instance-values :class :shift}
+         (fn? (get-in sut [:__instance_methods__ :add-instance-values])) => truthy
+         (get-in sut [:__instance_methods__ :class]) => :__class_symbol__
+         (fn? (get-in sut [:__instance_methods__ :shift])) => truthy))
+
 (facts "make Point"
        (let [sut (foc/make foc/Point -25 36)]
          (set (keys sut)) => #{:x :y :__class_symbol__}
+         (:__class_symbol__ 'Point)
          (:x sut) => -25
          (:y sut) => 36))
+
+(facts "send-to"
+       (let [sut (foc/make foc/Point -12 36)
+             shifted (foc/send-to sut :shift -5 -26)]
+         (set (keys shifted)) => #{:x :y :__class_symbol__}
+         (:__class_symbol__ 'Point)
+         (:x shifted) => -17
+         (:y shifted) => 10))
